18dedbb96e404e5dabe763a72242154c22522204
Niklas Therning <niklas@therning.org>
2013-04-24 16:21:41 +0200

    Fixed tabs -> spaces. (#82)

--- vm/rt/robovm/darwin_os_version.m
+++ vm/rt/robovm/darwin_os_version.m
@@ -1,18 +1,18 @@
 #import <Foundation/Foundation.h>
 
 void readDarwinOSVersionFromSystemVersionPList(char* buffer) {
-	assert(buffer != NULL);
+    assert(buffer != NULL);
 
-	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
 #if defined(IOS) && defined(RVM_X86)
-	// Prepend value of $IPHONE_SIMULATOR_ROOT environment variable
-	NSString* plistPath = [NSString stringWithFormat:@"%s/System/Library/CoreServices/SystemVersion.plist", getenv("IPHONE_SIMULATOR_ROOT")];
+    // Prepend value of $IPHONE_SIMULATOR_ROOT environment variable
+    NSString* plistPath = [NSString stringWithFormat:@"%s/System/Library/CoreServices/SystemVersion.plist", getenv("IPHONE_SIMULATOR_ROOT")];
 #else
-	NSString* plistPath = @"/System/Library/CoreServices/SystemVersion.plist";
+    NSString* plistPath = @"/System/Library/CoreServices/SystemVersion.plist";
 #endif
 
-	NSDictionary* plist = [NSDictionary dictionaryWithContentsOfFile:plistPath];
+    NSDictionary* plist = [NSDictionary dictionaryWithContentsOfFile:plistPath];
     NSString* version = [plist objectForKey:@"ProductVersion"];
     assert(version != nil);
     strcat(buffer, [version UTF8String]);

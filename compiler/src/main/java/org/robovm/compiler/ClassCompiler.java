da890d88ed9059bfdba1d5e91bda9235bba58fdd
Niklas Therning <niklas@therning.org>
2014-01-26 13:27:22 +0100

    Removed debug code.

--- compiler/src/main/java/org/robovm/compiler/ClassCompiler.java
+++ compiler/src/main/java/org/robovm/compiler/ClassCompiler.java
@@ -42,7 +42,6 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.robovm.compiler.clazz.Clazz;
 import org.robovm.compiler.clazz.ClazzInfo;
@@ -266,7 +265,7 @@ public class ClassCompiler {
     public void compile(Clazz clazz) throws IOException {
         reset();        
         
-        File llFile = config.getLlFile(clazz);
+//        File llFile = config.getLlFile(clazz);
 //        File bcFile = config.getBcFile(clazz);
 //        File sFile = config.getSFile(clazz);
         File oFile = config.getOFile(clazz);
@@ -292,7 +291,7 @@ public class ClassCompiler {
             }
             throw new RuntimeException(t);
         }
-FileUtils.writeByteArrayToFile(llFile, output.toByteArray());
+
         Context context = new Context();
         Module module = Module.parseIR(context, output.toByteArray(), clazz.getClassName());
         PassManager passManager = createPassManager();

b7800b7d5f6cc5c0af373286196d6948b719bf5d
BlueRiverTeam <blueriverteam@gmail.com>
2014-05-12 02:19:58 +0200

    Added MessageUI bindings.

---
+++ cocoatouch/src/main/java/org/robovm/apple/messageui/MFMailComposeViewController.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2014 Trillian Mobile AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.robovm.apple.messageui;
+
+/*<imports>*/
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+import org.robovm.objc.*;
+import org.robovm.objc.annotation.*;
+import org.robovm.objc.block.*;
+import org.robovm.rt.*;
+import org.robovm.rt.bro.*;
+import org.robovm.rt.bro.annotation.*;
+import org.robovm.rt.bro.ptr.*;
+import org.robovm.apple.foundation.*;
+import org.robovm.apple.uikit.*;
+/*</imports>*/
+
+/*<javadoc>*/
+
+/*</javadoc>*/
+/*<annotations>*/@Library("MessageUI") @NativeClass/*</annotations>*/
+/*<visibility>*/public/*</visibility>*/ class /*<name>*/MFMailComposeViewController/*</name>*/ 
+    extends /*<extends>*/UINavigationController/*</extends>*/ 
+    /*<implements>*//*</implements>*/ {
+
+    /*<ptr>*/public static class MFMailComposeViewControllerPtr extends Ptr<MFMailComposeViewController, MFMailComposeViewControllerPtr> {}/*</ptr>*/
+    /*<bind>*/static { ObjCRuntime.bind(MFMailComposeViewController.class); }/*</bind>*/
+    /*<constants>*//*</constants>*/
+    /*<constructors>*/
+    public MFMailComposeViewController() {}
+    protected MFMailComposeViewController(SkipInit skipInit) { super(skipInit); }
+    /*</constructors>*/
+    /*<properties>*/
+    @Property(selector = "mailComposeDelegate")
+    public native MFMailComposeViewControllerDelegate getMailComposeDelegate();
+    @Property(selector = "setMailComposeDelegate:", strongRef = true)
+    public native void setMailComposeDelegate(MFMailComposeViewControllerDelegate v);
+    /*</properties>*/
+    /*<members>*//*</members>*/
+    /*<methods>*/
+    /**
+     * @since Available in iOS 3.0 and later.
+     */
+    @GlobalValue(symbol="MFMailComposeErrorDomain", optional=true)
+    public static native NSString ErrorDomain();
+    
+    /**
+     * @since Available in iOS 3.0 and later.
+     */
+    @Method(selector = "setSubject:")
+    public native void setSubject(String subject);
+    /**
+     * @since Available in iOS 3.0 and later.
+     */
+    @Method(selector = "setToRecipients:")
+    public native void setToRecipients(NSArray<NSString> toRecipients);
+    /**
+     * @since Available in iOS 3.0 and later.
+     */
+    @Method(selector = "setCcRecipients:")
+    public native void setCcRecipients(NSArray<NSString> ccRecipients);
+    /**
+     * @since Available in iOS 3.0 and later.
+     */
+    @Method(selector = "setBccRecipients:")
+    public native void setBccRecipients(NSArray<NSString> bccRecipients);
+    /**
+     * @since Available in iOS 3.0 and later.
+     */
+    @Method(selector = "setMessageBody:isHTML:")
+    public native void setMessageBody(String body, boolean isHTML);
+    /**
+     * @since Available in iOS 3.0 and later.
+     */
+    @Method(selector = "addAttachmentData:mimeType:fileName:")
+    public native void addAttachmentData(NSData attachment, String mimeType, String filename);
+    /**
+     * @since Available in iOS 3.0 and later.
+     */
+    @Method(selector = "canSendMail")
+    public static native boolean canSendMail();
+    /*</methods>*/
+}
---
+++ cocoatouch/src/main/java/org/robovm/apple/messageui/MFMessageComposeViewController.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2014 Trillian Mobile AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.robovm.apple.messageui;
+
+/*<imports>*/
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+import org.robovm.objc.*;
+import org.robovm.objc.annotation.*;
+import org.robovm.objc.block.*;
+import org.robovm.rt.*;
+import org.robovm.rt.bro.*;
+import org.robovm.rt.bro.annotation.*;
+import org.robovm.rt.bro.ptr.*;
+import org.robovm.apple.foundation.*;
+import org.robovm.apple.uikit.*;
+/*</imports>*/
+
+/*<javadoc>*/
+/**
+ * @since Available in iOS 4.0 and later.
+ */
+/*</javadoc>*/
+/*<annotations>*/@Library("MessageUI") @NativeClass/*</annotations>*/
+/*<visibility>*/public/*</visibility>*/ class /*<name>*/MFMessageComposeViewController/*</name>*/ 
+    extends /*<extends>*/UINavigationController/*</extends>*/ 
+    /*<implements>*//*</implements>*/ {
+
+    /*<ptr>*/public static class MFMessageComposeViewControllerPtr extends Ptr<MFMessageComposeViewController, MFMessageComposeViewControllerPtr> {}/*</ptr>*/
+    /*<bind>*/static { ObjCRuntime.bind(MFMessageComposeViewController.class); }/*</bind>*/
+    /*<constants>*//*</constants>*/
+    /*<constructors>*/
+    public MFMessageComposeViewController() {}
+    protected MFMessageComposeViewController(SkipInit skipInit) { super(skipInit); }
+    /*</constructors>*/
+    /*<properties>*/
+    @Property(selector = "messageComposeDelegate")
+    public native MFMessageComposeViewControllerDelegate getMessageComposeDelegate();
+    @Property(selector = "setMessageComposeDelegate:", strongRef = true)
+    public native void setMessageComposeDelegate(MFMessageComposeViewControllerDelegate v);
+    @Property(selector = "recipients")
+    public native NSArray<NSString> getRecipients();
+    @Property(selector = "setRecipients:")
+    public native void setRecipients(NSArray<NSString> v);
+    @Property(selector = "body")
+    public native String getBody();
+    @Property(selector = "setBody:")
+    public native void setBody(String v);
+    @Property(selector = "subject")
+    public native String getSubject();
+    @Property(selector = "setSubject:")
+    public native void setSubject(String v);
+    @Property(selector = "attachments")
+    public native NSArray<NSDictionary<NSString, ?>> getAttachments();
+    /*</properties>*/
+    /*<members>*//*</members>*/
+    /*<methods>*/
+    @GlobalValue(symbol="MFMessageComposeViewControllerAttachmentURL", optional=true)
+    public static native NSString AttachmentURL();
+    @GlobalValue(symbol="MFMessageComposeViewControllerAttachmentAlternateFilename", optional=true)
+    public static native NSString AttachmentAlternateFilename();
+    /**
+     * @since Available in iOS 5.0 and later.
+     */
+    @GlobalValue(symbol="MFMessageComposeViewControllerTextMessageAvailabilityDidChangeNotification", optional=true)
+    public static native NSString TextMessageAvailabilityDidChangeNotification();
+    /**
+     * @since Available in iOS 5.0 and later.
+     */
+    @GlobalValue(symbol="MFMessageComposeViewControllerTextMessageAvailabilityKey", optional=true)
+    public static native NSString TextMessageAvailabilityKey();
+    
+    /**
+     * @since Available in iOS 7.0 and later.
+     */
+    @Method(selector = "disableUserAttachments")
+    public native void disableUserAttachments();
+    /**
+     * @since Available in iOS 7.0 and later.
+     */
+    @Method(selector = "addAttachmentURL:withAlternateFilename:")
+    public native boolean addAttachment(NSURL attachmentURL, String alternateFilename);
+    /**
+     * @since Available in iOS 7.0 and later.
+     */
+    @Method(selector = "addAttachmentData:typeIdentifier:filename:")
+    public native boolean addAttachment(NSData attachmentData, String uti, String filename);
+    /**
+     * @since Available in iOS 4.0 and later.
+     */
+    @Method(selector = "canSendText")
+    public static native boolean canSendText();
+    /**
+     * @since Available in iOS 7.0 and later.
+     */
+    @Method(selector = "canSendSubject")
+    public static native boolean canSendSubject();
+    /**
+     * @since Available in iOS 7.0 and later.
+     */
+    @Method(selector = "canSendAttachments")
+    public static native boolean canSendAttachments();
+    /**
+     * @since Available in iOS 7.0 and later.
+     */
+    @Method(selector = "isSupportedAttachmentUTI:")
+    public static native boolean isSupportedAttachmentUTI(String uti);
+    /*</methods>*/
+}

6fc7085cf0583bb781c9f63acebcaabd971573e3
BlueRiverTeam <blueriverteam@gmail.com>
2014-06-23 23:59:18 +0200

    Added MultipeerConnectivity bindings. (#289)

---
+++ cocoatouch/src/main/java/org/robovm/apple/multipeerconnectivity/MCAdvertiserAssistant.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2014 Trillian Mobile AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.robovm.apple.multipeerconnectivity;
+
+/*<imports>*/
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+import org.robovm.objc.*;
+import org.robovm.objc.annotation.*;
+import org.robovm.objc.block.*;
+import org.robovm.rt.*;
+import org.robovm.rt.bro.*;
+import org.robovm.rt.bro.annotation.*;
+import org.robovm.rt.bro.ptr.*;
+import org.robovm.apple.foundation.*;
+import org.robovm.apple.uikit.*;
+/*</imports>*/
+
+/*<javadoc>*/
+/**
+ * @since Available in iOS 7.0 and later.
+ */
+/*</javadoc>*/
+/*<annotations>*/@Library("MultipeerConnectivity") @NativeClass/*</annotations>*/
+/*<visibility>*/public/*</visibility>*/ class /*<name>*/MCAdvertiserAssistant/*</name>*/ 
+    extends /*<extends>*/NSObject/*</extends>*/ 
+    /*<implements>*//*</implements>*/ {
+
+    /*<ptr>*/public static class MCAdvertiserAssistantPtr extends Ptr<MCAdvertiserAssistant, MCAdvertiserAssistantPtr> {}/*</ptr>*/
+    /*<bind>*/static { ObjCRuntime.bind(MCAdvertiserAssistant.class); }/*</bind>*/
+    /*<constants>*//*</constants>*/
+    /*<constructors>*/
+    public MCAdvertiserAssistant() {}
+    protected MCAdvertiserAssistant(SkipInit skipInit) { super(skipInit); }
+    public MCAdvertiserAssistant(String serviceType, NSDictionary<NSString, NSString> info, MCSession session) { super((SkipInit) null); initObject(init(serviceType, info, session)); }
+    /*</constructors>*/
+    /*<properties>*/
+    @Property(selector = "delegate")
+    public native MCAdvertiserAssistantDelegate getDelegate();
+    @Property(selector = "setDelegate:", strongRef = true)
+    public native void setDelegate(MCAdvertiserAssistantDelegate v);
+    @Property(selector = "session")
+    public native MCSession getSession();
+    @Property(selector = "discoveryInfo")
+    public native NSDictionary<NSString, NSString> getDiscoveryInfo();
+    @Property(selector = "serviceType")
+    public native String getServiceType();
+    /*</properties>*/
+    /*<members>*//*</members>*/
+    /*<methods>*/
+    @Method(selector = "initWithServiceType:discoveryInfo:session:")
+    protected native @Pointer long init(String serviceType, NSDictionary<NSString, NSString> info, MCSession session);
+    @Method(selector = "start")
+    public native void start();
+    @Method(selector = "stop")
+    public native void stop();
+    /*</methods>*/
+}
---
+++ cocoatouch/src/main/java/org/robovm/apple/multipeerconnectivity/MCSession.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2014 Trillian Mobile AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.robovm.apple.multipeerconnectivity;
+
+/*<imports>*/
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+import org.robovm.objc.*;
+import org.robovm.objc.annotation.*;
+import org.robovm.objc.block.*;
+import org.robovm.rt.*;
+import org.robovm.rt.bro.*;
+import org.robovm.rt.bro.annotation.*;
+import org.robovm.rt.bro.ptr.*;
+import org.robovm.apple.foundation.*;
+import org.robovm.apple.uikit.*;
+/*</imports>*/
+
+/*<javadoc>*/
+/**
+ * @since Available in iOS 7.0 and later.
+ */
+/*</javadoc>*/
+/*<annotations>*/@Library("MultipeerConnectivity") @NativeClass/*</annotations>*/
+/*<visibility>*/public/*</visibility>*/ class /*<name>*/MCSession/*</name>*/ 
+    extends /*<extends>*/NSObject/*</extends>*/ 
+    /*<implements>*//*</implements>*/ {
+
+    /*<ptr>*/public static class MCSessionPtr extends Ptr<MCSession, MCSessionPtr> {}/*</ptr>*/
+    /*<bind>*/static { ObjCRuntime.bind(MCSession.class); }/*</bind>*/
+    /*<constants>*//*</constants>*/
+    /*<constructors>*/
+    public MCSession() {}
+    protected MCSession(SkipInit skipInit) { super(skipInit); }
+    public MCSession(MCPeerID myPeerID) { super((SkipInit) null); initObject(init(myPeerID)); }
+    public MCSession(MCPeerID myPeerID, NSArray<?> identity, MCEncryptionPreference encryptionPreference) { super((SkipInit) null); initObject(init(myPeerID, identity, encryptionPreference)); }
+    /*</constructors>*/
+    /*<properties>*/
+    @Property(selector = "delegate")
+    public native MCSessionDelegate getDelegate();
+    @Property(selector = "setDelegate:", strongRef = true)
+    public native void setDelegate(MCSessionDelegate v);
+    @Property(selector = "myPeerID")
+    public native MCPeerID getMyPeerID();
+    @Property(selector = "securityIdentity")
+    public native NSArray<?> getSecurityIdentity();
+    @Property(selector = "encryptionPreference")
+    public native MCEncryptionPreference getEncryptionPreference();
+    @Property(selector = "connectedPeers")
+    public native NSArray<MCPeerID> getConnectedPeers();
+    /*</properties>*/
+    /*<members>*//*</members>*/
+    /*<methods>*/
+    @Method(selector = "initWithPeer:")
+    protected native @Pointer long init(MCPeerID myPeerID);
+    @Method(selector = "initWithPeer:securityIdentity:encryptionPreference:")
+    protected native @Pointer long init(MCPeerID myPeerID, NSArray<?> identity, MCEncryptionPreference encryptionPreference);
+    @Method(selector = "sendData:toPeers:withMode:error:")
+    public native boolean sendData(NSData data, NSArray<MCPeerID> peerIDs, MCSessionSendDataMode mode, NSError.NSErrorPtr error);
+    @Method(selector = "disconnect")
+    public native void disconnect();
+    @Method(selector = "sendResourceAtURL:withName:toPeer:withCompletionHandler:")
+    public native NSProgress sendResource(NSURL resourceURL, String resourceName, MCPeerID peerID, @Block VoidBlock1<NSError> completionHandler);
+    @Method(selector = "startStreamWithName:toPeer:error:")
+    public native NSOutputStream startStream(String streamName, MCPeerID peerID, NSError.NSErrorPtr error);
+    @Method(selector = "nearbyConnectionDataForPeer:withCompletionHandler:")
+    public native void requestNearbyConnectionData(MCPeerID peerID, @Block VoidBlock2<NSData, NSError> completionHandler);
+    @Method(selector = "connectPeer:withNearbyConnectionData:")
+    public native void connectPeer(MCPeerID peerID, NSData data);
+    @Method(selector = "cancelConnectPeer:")
+    public native void cancelConnectPeer(MCPeerID peerID);
+    /*</methods>*/
+}

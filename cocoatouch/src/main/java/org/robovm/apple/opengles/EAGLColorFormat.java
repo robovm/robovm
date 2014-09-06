a43ceb12f8e897e4a0bbb4b435756693e5435640
BlueRiverTeam <blueriverteam@gmail.com>
2014-09-06 20:08:43 +0200

    Wrap global values in OpenGLES (#477).

---
+++ cocoatouch/src/main/java/org/robovm/apple/opengles/EAGLColorFormat.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2014 Trillian Mobile AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.robovm.apple.opengles;
+
+/*<imports>*/
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+import org.robovm.objc.*;
+import org.robovm.objc.annotation.*;
+import org.robovm.objc.block.*;
+import org.robovm.rt.*;
+import org.robovm.rt.bro.*;
+import org.robovm.rt.bro.annotation.*;
+import org.robovm.rt.bro.ptr.*;
+import org.robovm.apple.foundation.*;
+/*</imports>*/
+
+/*<javadoc>*/
+/*</javadoc>*/
+/*<annotations>*/@Library("OpenGLES")/*</annotations>*/
+/*<visibility>*/public/*</visibility>*/ class /*<name>*/EAGLColorFormat/*</name>*/ 
+    extends /*<extends>*/Object/*</extends>*/ 
+    /*<implements>*//*</implements>*/ {
+
+    /*<ptr>*/
+    /*</ptr>*/
+    /*<bind>*/static { Bro.bind(EAGLColorFormat.class); }/*</bind>*/
+    /*<constants>*//*</constants>*/
+    public static EAGLColorFormat RGBA8 = new EAGLColorFormat("RGBA8Value");
+    public static EAGLColorFormat RGB565 = new EAGLColorFormat("RGB565Value");
+    /**
+     * @since Available in iOS 7.0 and later.
+     */
+    public static EAGLColorFormat SRGBA8 = new EAGLColorFormat("SRGBA8Value");
+    private static EAGLColorFormat[] values = new EAGLColorFormat[] {RGBA8, RGB565, SRGBA8};
+    
+    private final LazyGlobalValue<NSString> lazyGlobalValue;
+    
+    private EAGLColorFormat(String getterName) {
+        lazyGlobalValue = new LazyGlobalValue<>(getClass(), getterName);
+    }
+    /*<constructors>*//*</constructors>*/
+    /*<properties>*//*</properties>*/
+    /*<members>*//*</members>*/
+    public NSString value() {
+        return lazyGlobalValue.value();
+    }
+    
+    public static EAGLColorFormat valueOf(NSString value) {
+        for (EAGLColorFormat v : values) {
+            if (v.value().equals(value)) {
+                return v;
+            }
+        }
+        throw new IllegalArgumentException("No constant with value " + value + " found in " 
+            + /*<name>*/EAGLColorFormat/*</name>*/.class.getName());
+    }
+    /*<methods>*/
+    @GlobalValue(symbol="kEAGLColorFormatRGBA8", optional=true)
+    protected static native NSString RGBA8Value();
+    @GlobalValue(symbol="kEAGLColorFormatRGB565", optional=true)
+    protected static native NSString RGB565Value();
+    /**
+     * @since Available in iOS 7.0 and later.
+     */
+    @GlobalValue(symbol="kEAGLColorFormatSRGBA8", optional=true)
+    protected static native NSString SRGBA8Value();
+    /*</methods>*/
+}
---
+++ cocoatouch/src/main/java/org/robovm/apple/opengles/EAGLDrawableProperties.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2014 Trillian Mobile AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.robovm.apple.opengles;
+
+/*<imports>*/
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+
+import org.robovm.objc.*;
+import org.robovm.objc.annotation.*;
+import org.robovm.objc.block.*;
+import org.robovm.rt.*;
+import org.robovm.rt.bro.*;
+import org.robovm.rt.bro.annotation.*;
+import org.robovm.rt.bro.ptr.*;
+import org.robovm.apple.foundation.*;
+/*</imports>*/
+
+/*<javadoc>*/
+/*</javadoc>*/
+/*<annotations>*/@Library("OpenGLES")/*</annotations>*/
+/*<visibility>*/public/*</visibility>*/ class /*<name>*/EAGLDrawableProperties/*</name>*/ 
+    extends /*<extends>*/Object/*</extends>*/ 
+    /*<implements>*//*</implements>*/ {
+
+    /*<ptr>*/
+    /*</ptr>*/
+    private NSDictionary<NSString, NSObject> data;
+    
+    public EAGLDrawableProperties() {
+        this.data = new NSMutableDictionary<NSString, NSObject>();
+    }
+    protected EAGLDrawableProperties(NSDictionary<NSString, NSObject> data) {
+        this.data = data;
+    }
+    /*<bind>*/static { Bro.bind(EAGLDrawableProperties.class); }/*</bind>*/
+    /*<constants>*//*</constants>*/
+    /*<constructors>*//*</constructors>*/
+    /*<properties>*//*</properties>*/
+    /*<members>*//*</members>*/
+    public NSDictionary<NSString, NSObject> getDictionary() {
+        return data;
+    }
+    public boolean isRetainedBacking() {
+        if (data.containsKey(RetainedBacking())) {
+            NSNumber val = (NSNumber)data.get(RetainedBacking());
+            return val.booleanValue();
+        }
+        return false;
+    }
+    public void setRetainedBacking(boolean retainedBacking) {
+        data.put(RetainedBacking(), NSNumber.valueOf(retainedBacking));
+    }
+    public EAGLColorFormat getColorFormat() {
+        if (data.containsKey(ColorFormat())) {
+            NSString val = (NSString)data.get(ColorFormat());
+            return EAGLColorFormat.valueOf(val);
+        }
+        return EAGLColorFormat.RGBA8;
+    }
+    public void setColorFormat(EAGLColorFormat colorFormat) {
+        data.put(ColorFormat(), colorFormat.value());
+    }
+    /*<methods>*/
+    @GlobalValue(symbol="kEAGLDrawablePropertyRetainedBacking", optional=true)
+    protected static native NSString RetainedBacking();
+    @GlobalValue(symbol="kEAGLDrawablePropertyColorFormat", optional=true)
+    protected static native NSString ColorFormat();
+    /*</methods>*/
+}

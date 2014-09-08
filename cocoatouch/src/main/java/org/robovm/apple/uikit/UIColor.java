d3a8636274ebe373f1b8452fdc2fb284f724422f
BlueRiverTeam <blueriverteam@gmail.com>
2014-09-08 02:12:41 +0200

    Changed UIColor method names (#478).

--- cocoatouch/src/main/java/org/robovm/apple/uikit/UIColor.java
+++ cocoatouch/src/main/java/org/robovm/apple/uikit/UIColor.java
@@ -67,57 +67,51 @@ import org.robovm.apple.coreimage.*;
     /*</properties>*/
     /*<members>*//*</members>*/
     
-    public boolean getWhiteAlpha(double[] whiteAlpha) {
-        if (whiteAlpha == null) {
-            throw new NullPointerException("whiteAlpha");
-        }
-        if (whiteAlpha.length != 2) {
-            throw new IllegalArgumentException("whiteAlpha.length != 2");
-        }
+    public double[] getWhiteAlpha() {
         MachineSizedFloatPtr white = new MachineSizedFloatPtr();
         MachineSizedFloatPtr alpha = new MachineSizedFloatPtr();
         boolean ret = getWhiteAlpha(white, alpha);
-        whiteAlpha[0] = white.get();
-        whiteAlpha[1] = alpha.get();
-        return ret;
+        if (ret) {
+            double[] whiteAlpha = new double[2];
+            whiteAlpha[0] = white.get();
+            whiteAlpha[1] = alpha.get();
+            return whiteAlpha;
+        }
+        return null;
     }
 
-    public boolean getHSBA(double[] hsba) {
-        if (hsba == null) {
-            throw new NullPointerException("hsba");
-        }
-        if (hsba.length != 4) {
-            throw new IllegalArgumentException("hsba.length != 4");
-        }
+    public double[] getHSBA() {
         MachineSizedFloatPtr hue = new MachineSizedFloatPtr();
         MachineSizedFloatPtr saturation = new MachineSizedFloatPtr();
         MachineSizedFloatPtr brightness = new MachineSizedFloatPtr();
         MachineSizedFloatPtr alpha = new MachineSizedFloatPtr();
         boolean ret = getHSBA(hue, saturation, brightness, alpha);
-        hsba[0] = hue.get();
-        hsba[1] = saturation.get();
-        hsba[2] = brightness.get();
-        hsba[3] = alpha.get();
-        return ret;
+        if (ret) {
+            double[] hsba = new double[4];
+            hsba[0] = hue.get();
+            hsba[1] = saturation.get();
+            hsba[2] = brightness.get();
+            hsba[3] = alpha.get();
+            return hsba;
+        }
+        return null;
     }
 
-    public boolean getRGBA(double[] rgba) {
-        if (rgba == null) {
-            throw new NullPointerException("rgba");
-        }
-        if (rgba.length != 4) {
-            throw new IllegalArgumentException("rgba.length != 4");
-        }
+    public double[] getRGBA() {
         MachineSizedFloatPtr red = new MachineSizedFloatPtr();
         MachineSizedFloatPtr green = new MachineSizedFloatPtr();
         MachineSizedFloatPtr blue = new MachineSizedFloatPtr();
         MachineSizedFloatPtr alpha = new MachineSizedFloatPtr();
         boolean ret = getRGBA(red, green, blue, alpha);
-        rgba[0] = red.get();
-        rgba[1] = green.get();
-        rgba[2] = blue.get();
-        rgba[3] = alpha.get();
-        return ret;
+        if (ret) {
+            double[] rgba = new double[4];
+            rgba[0] = red.get();
+            rgba[1] = green.get();
+            rgba[2] = blue.get();
+            rgba[3] = alpha.get();
+            return rgba;
+        }
+        return null;
     }
     
     /*<methods>*/
@@ -154,76 +148,76 @@ import org.robovm.apple.coreimage.*;
     @Method(selector = "colorWithAlphaComponent:")
     public native UIColor addAlpha(@MachineSizedFloat double alpha);
     @Method(selector = "colorWithWhite:alpha:")
-    public static native UIColor createFromWhiteAlpha(@MachineSizedFloat double white, @MachineSizedFloat double alpha);
+    public static native UIColor fromWhiteAlpha(@MachineSizedFloat double white, @MachineSizedFloat double alpha);
     @Method(selector = "colorWithHue:saturation:brightness:alpha:")
-    public static native UIColor createFromHSBA(@MachineSizedFloat double hue, @MachineSizedFloat double saturation, @MachineSizedFloat double brightness, @MachineSizedFloat double alpha);
+    public static native UIColor fromHSBA(@MachineSizedFloat double hue, @MachineSizedFloat double saturation, @MachineSizedFloat double brightness, @MachineSizedFloat double alpha);
     @Method(selector = "colorWithRed:green:blue:alpha:")
-    public static native UIColor createFromRGBA(@MachineSizedFloat double red, @MachineSizedFloat double green, @MachineSizedFloat double blue, @MachineSizedFloat double alpha);
+    public static native UIColor fromRGBA(@MachineSizedFloat double red, @MachineSizedFloat double green, @MachineSizedFloat double blue, @MachineSizedFloat double alpha);
     @Method(selector = "colorWithCGColor:")
-    public static native UIColor createFromCGColor(CGColor cgColor);
+    public static native UIColor fromCGColor(CGColor cgColor);
     @Method(selector = "colorWithPatternImage:")
-    public static native UIColor createFromPatternImage(UIImage image);
+    public static native UIColor fromPatternImage(UIImage image);
     /**
      * @since Available in iOS 5.0 and later.
      */
     @Method(selector = "colorWithCIColor:")
-    public static native UIColor createFromCIColor(CIColor ciColor);
+    public static native UIColor fromCIColor(CIColor ciColor);
     @Method(selector = "blackColor")
-    public static native UIColor colorBlack();
+    public static native UIColor black();
     @Method(selector = "darkGrayColor")
-    public static native UIColor colorDarkGray();
+    public static native UIColor darkGray();
     @Method(selector = "lightGrayColor")
-    public static native UIColor colorLightGray();
+    public static native UIColor lightGray();
     @Method(selector = "whiteColor")
-    public static native UIColor colorWhite();
+    public static native UIColor white();
     @Method(selector = "grayColor")
-    public static native UIColor colorGray();
+    public static native UIColor gray();
     @Method(selector = "redColor")
-    public static native UIColor colorRed();
+    public static native UIColor red();
     @Method(selector = "greenColor")
-    public static native UIColor colorGreen();
+    public static native UIColor green();
     @Method(selector = "blueColor")
-    public static native UIColor colorBlue();
+    public static native UIColor blue();
     @Method(selector = "cyanColor")
-    public static native UIColor colorCyan();
+    public static native UIColor cyan();
     @Method(selector = "yellowColor")
-    public static native UIColor colorYellow();
+    public static native UIColor yellow();
     @Method(selector = "magentaColor")
-    public static native UIColor colorMagenta();
+    public static native UIColor magenta();
     @Method(selector = "orangeColor")
-    public static native UIColor colorOrange();
+    public static native UIColor orange();
     @Method(selector = "purpleColor")
-    public static native UIColor colorPurple();
+    public static native UIColor purple();
     @Method(selector = "brownColor")
-    public static native UIColor colorBrown();
+    public static native UIColor brown();
     @Method(selector = "clearColor")
-    public static native UIColor colorClear();
+    public static native UIColor clear();
     @Method(selector = "lightTextColor")
-    public static native UIColor colorLightText();
+    public static native UIColor lightText();
     @Method(selector = "darkTextColor")
-    public static native UIColor colorDarkText();
+    public static native UIColor darkText();
     @Method(selector = "groupTableViewBackgroundColor")
-    public static native UIColor colorGroupTableViewBackground();
+    public static native UIColor groupTableViewBackgroundColor();
     /**
      * @since Available in iOS 2.0 and later.
      * @deprecated Deprecated in iOS 7.0.
      */
     @Deprecated
     @Method(selector = "viewFlipsideBackgroundColor")
-    public static native UIColor colorViewFlipsideBackground();
+    public static native UIColor viewFlipsideBackgroundColor();
     /**
      * @since Available in iOS 3.2 and later.
      * @deprecated Deprecated in iOS 7.0.
      */
     @Deprecated
     @Method(selector = "scrollViewTexturedBackgroundColor")
-    public static native UIColor colorScrollViewTexturedBackground();
+    public static native UIColor scrollViewTexturedBackgroundColor();
     /**
      * @since Available in iOS 5.0 and later.
      * @deprecated Deprecated in iOS 7.0.
      */
     @Deprecated
     @Method(selector = "underPageBackgroundColor")
-    public static native UIColor colorUnderPageBackground();
+    public static native UIColor underPageBackgroundColor();
     /*</methods>*/
 }

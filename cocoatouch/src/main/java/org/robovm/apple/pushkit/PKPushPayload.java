320a5d7b870859b8df7b099164939ee01988168b
BlueRiverTeam <blueriverteam@gmail.com>
2014-11-15 22:16:53 +0100

    Bind PushKit (#585).

---
+++ cocoatouch/src/main/java/org/robovm/apple/pushkit/PKPushPayload.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2014 Trillian Mobile AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.robovm.apple.pushkit;
+
+/*<imports>*/
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+import org.robovm.objc.*;
+import org.robovm.objc.annotation.*;
+import org.robovm.objc.block.*;
+import org.robovm.rt.*;
+import org.robovm.rt.bro.*;
+import org.robovm.rt.bro.annotation.*;
+import org.robovm.rt.bro.ptr.*;
+import org.robovm.apple.foundation.*;
+import org.robovm.apple.dispatch.*;
+/*</imports>*/
+
+/*<javadoc>*/
+/**
+ * @since Available in iOS 8.0 and later.
+ */
+/*</javadoc>*/
+/*<annotations>*/@Library("PushKit") @NativeClass/*</annotations>*/
+/*<visibility>*/public/*</visibility>*/ class /*<name>*/PKPushPayload/*</name>*/ 
+    extends /*<extends>*/NSObject/*</extends>*/ 
+    /*<implements>*//*</implements>*/ {
+
+    /*<ptr>*/public static class PKPushPayloadPtr extends Ptr<PKPushPayload, PKPushPayloadPtr> {}/*</ptr>*/
+    /*<bind>*/static { ObjCRuntime.bind(PKPushPayload.class); }/*</bind>*/
+    /*<constants>*//*</constants>*/
+    /*<constructors>*/
+    public PKPushPayload() {}
+    protected PKPushPayload(SkipInit skipInit) { super(skipInit); }
+    /*</constructors>*/
+    /*<properties>*/
+    @Property(selector = "type")
+    public native String getType();
+    @Property(selector = "dictionaryPayload")
+    public native NSDictionary<?, ?> getDictionaryPayload();
+    /*</properties>*/
+    /*<members>*//*</members>*/
+    /*<methods>*/
+    
+    /*</methods>*/
+}
---
+++ cocoatouch/src/main/java/org/robovm/apple/pushkit/PKPushRegistry.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2014 Trillian Mobile AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.robovm.apple.pushkit;
+
+/*<imports>*/
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+import org.robovm.objc.*;
+import org.robovm.objc.annotation.*;
+import org.robovm.objc.block.*;
+import org.robovm.rt.*;
+import org.robovm.rt.bro.*;
+import org.robovm.rt.bro.annotation.*;
+import org.robovm.rt.bro.ptr.*;
+import org.robovm.apple.foundation.*;
+import org.robovm.apple.dispatch.*;
+/*</imports>*/
+
+/*<javadoc>*/
+/**
+ * @since Available in iOS 8.0 and later.
+ */
+/*</javadoc>*/
+/*<annotations>*/@Library("PushKit") @NativeClass/*</annotations>*/
+/*<visibility>*/public/*</visibility>*/ class /*<name>*/PKPushRegistry/*</name>*/ 
+    extends /*<extends>*/NSObject/*</extends>*/ 
+    /*<implements>*//*</implements>*/ {
+
+    /*<ptr>*/public static class PKPushRegistryPtr extends Ptr<PKPushRegistry, PKPushRegistryPtr> {}/*</ptr>*/
+    /*<bind>*/static { ObjCRuntime.bind(PKPushRegistry.class); }/*</bind>*/
+    /*<constants>*//*</constants>*/
+    /*<constructors>*/
+    public PKPushRegistry() {}
+    protected PKPushRegistry(SkipInit skipInit) { super(skipInit); }
+    public PKPushRegistry(DispatchQueue queue) { super((SkipInit) null); initObject(init(queue)); }
+    /*</constructors>*/
+    /*<properties>*/
+    @Property(selector = "delegate")
+    public native PKPushRegistryDelegate getDelegate();
+    @Property(selector = "setDelegate:", strongRef = true)
+    public native void setDelegate(PKPushRegistryDelegate v);
+    @Property(selector = "desiredPushTypes")
+    public native @org.robovm.rt.bro.annotation.Marshaler(NSSet.AsStringSetMarshaler.class) Set<String> getDesiredPushTypes();
+    @Property(selector = "setDesiredPushTypes:")
+    public native void setDesiredPushTypes(@org.robovm.rt.bro.annotation.Marshaler(NSSet.AsStringSetMarshaler.class) Set<String> v);
+    /*</properties>*/
+    /*<members>*//*</members>*/
+    /*<methods>*/
+    @Method(selector = "pushTokenForType:")
+    public native NSData getPushTokenForType(String type);
+    @Method(selector = "initWithQueue:")
+    protected native @Pointer long init(DispatchQueue queue);
+    /*</methods>*/
+}

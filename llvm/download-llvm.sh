287d2130e4d8eefe54b588f1fd3201872e8b7b59
Niklas Therning <niklas@therning.org>
2014-12-30 16:16:27 +0100

    Removed llvm/download-llvm.sh script

--- llvm/download-llvm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf llvm-3.5.0.src.tar*
-curl -O http://llvm.org/releases/3.5.0/llvm-3.5.0.src.tar.xz
-unxz llvm-3.5.0.src.tar.xz
-tar xvf llvm-3.5.0.src.tar -C extllvm --strip-components 1

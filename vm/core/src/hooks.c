237316bdb113752b4b735f540beb8fa9f4bfc50f
badlogic <badlogicgames@gmail.com>
2015-02-19 21:29:57 +0100

    Handling skipping of exceptions from VMClassLoader#findClassInClasspathForLoader in hooks.c. No round trips, faster startup time

--- vm/core/src/hooks.c
+++ vm/core/src/hooks.c
@@ -22,6 +22,7 @@
 #include <string.h>
 #include <errno.h>
 #include <netinet/tcp.h>
+#include <robovm/types.h>
 #include "private.h"
 
 #define LOG_TAG "hooks"
@@ -1299,11 +1300,29 @@ void _rvmHookInstrumented(DebugEnv* debugEnv, jint lineNumber, jint lineNumberOf
 void _rvmHookExceptionRaised(Env* env, Object* throwable) {
     DebugEnv* debugEnv = (DebugEnv*)env;
 
-    return;
-
     // we need to temporarily clear the exception
     // for the code below to not get upset.
     Object* exception = rvmExceptionClear(env);
+
+    // special case for VMClassLoader, we don't
+    // report exceptions thrown by it.
+    CallStack* callStack = rvmCaptureCallStack(env);
+    jint index = 0;
+    jint length = 0;
+    jint classNamesSize = 0;
+    CallStackFrame* frame = NULL;
+    frame = rvmGetNextCallStackMethod(env, callStack, &index);
+    if(frame == NULL) {
+        DEBUG("No frames for exception event, resuming");
+        rvmThrow(env, exception);
+        return;
+    }
+    if(!strcmp(frame->method->name, "findClassInClasspathForLoader") &&
+            !strcmp(frame->method->clazz->name, "java/lang/VMClassLoader")) {
+        DEBUG("Skipping ClassNotFoundException from VMClassLoader#findClassInClasspathForLoader");
+        rvmThrow(env, exception);
+        return;
+    }
 
     rvmPushGatewayFrame(env);
     rvmLockMutex(&debugEnv->suspendMutex);
@@ -1313,7 +1332,6 @@ void _rvmHookExceptionRaised(Env* env, Object* throwable) {
                 env->currentThread->threadId, throwable->clazz->name);
     }
 
-    CallStack* callStack = rvmCaptureCallStack(env);
     if (rvmExceptionCheck(env)) {
         Object* ex = rvmExceptionClear(env);
         ERRORF("Failed to get a call stack for thread %p due to exception event. Got an exception of type: %s",

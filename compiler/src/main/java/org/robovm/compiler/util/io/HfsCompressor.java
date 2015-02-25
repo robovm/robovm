bb7fc8e67dcbcc7b1f856875558b82b9e7e99350
badlogic <badlogicgames@gmail.com>
2015-02-25 22:13:02 +0100

    Fixed header in HfsCompressor

--- compiler/src/main/java/org/robovm/compiler/util/io/HfsCompressor.java
+++ compiler/src/main/java/org/robovm/compiler/util/io/HfsCompressor.java
@@ -1,14 +1,18 @@
-/* Copyright (c) 2014 Reinhard Pointner, All Rights Reserved
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
+/*
+ * Copyright (C) 2012 Trillian Mobile AB
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/gpl-2.0.html>.
  */
 package org.robovm.compiler.util.io;
 

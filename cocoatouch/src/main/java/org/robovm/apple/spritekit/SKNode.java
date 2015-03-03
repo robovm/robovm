90386fe712499c8e9cd7f7fc7fa9d655dc9af527
BlueRiverInteractive <blueriverteam@gmail.com>
2015-03-03 11:24:44 +0100

    Use the new BooleanPtr for blocks in SpriteKit.

--- cocoatouch/src/main/java/org/robovm/apple/spritekit/SKNode.java
+++ cocoatouch/src/main/java/org/robovm/apple/spritekit/SKNode.java
@@ -147,7 +147,7 @@ import org.robovm.apple.scenekit.*;
     @Method(selector = "childNodeWithName:")
     public native SKNode getChild(String name);
     @Method(selector = "enumerateChildNodesWithName:usingBlock:")
-    public native void enumerateChildNodes(String name, @Block VoidBlock2<SKNode, BytePtr> block);
+    public native void enumerateChildNodes(String name, @Block VoidBlock2<SKNode, BooleanPtr> block);
     /**
      * @since Available in iOS 8.0 and later.
      */

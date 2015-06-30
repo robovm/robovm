ec25920b8c9510df0fb3947f283278f60f053456
Niklas Therning <niklas@therning.org>
2015-06-30 17:28:16 +0200

    Fix compilation issue on Linux which needs _GNU_SOURCE to be defined for dladdr() to be visible.

--- vm/core/src/init.c
+++ vm/core/src/init.c
@@ -13,6 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#if defined(LINUX)
+#   define _GNU_SOURCE // Required on Linux to get dladdr() support
+#endif
+
 #include <robovm.h>
 #include <string.h>
 #include <stdio.h>

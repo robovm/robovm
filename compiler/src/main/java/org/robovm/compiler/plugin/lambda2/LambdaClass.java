12752500bdaa71c72e92d822eb3049619ab2a3f2
badlogic <badlogicgames@gmail.com>
2015-07-23 14:25:04 +0200

    Fixed formatting of LambdaClass

--- compiler/src/main/java/org/robovm/compiler/plugin/lambda2/LambdaClass.java
+++ compiler/src/main/java/org/robovm/compiler/plugin/lambda2/LambdaClass.java
@@ -20,41 +20,39 @@ import java.util.List;
 
 import soot.Type;
 
-/**
- * Created by badlogic on 09/07/15.
- */
 public class LambdaClass {
-    private final String lambdaClassName;
-    private final byte[] classData;
-    private final String targetMethodName;
-    private final List<Type> targetMethodParameters;
-    private final Type targetMethodReturnType;
+	private final String lambdaClassName;
+	private final byte[] classData;
+	private final String targetMethodName;
+	private final List<Type> targetMethodParameters;
+	private final Type targetMethodReturnType;
 
-    public LambdaClass(String lambdaClassName, byte[] classData, String targetMethodName, List<Type> targetMethodParameters, Type targetMethodReturnType) {
-        this.lambdaClassName = lambdaClassName;
-        this.classData = classData;
-        this.targetMethodName = targetMethodName;
-        this.targetMethodParameters = targetMethodParameters;
-        this.targetMethodReturnType = targetMethodReturnType;
-    }
+	public LambdaClass(String lambdaClassName, byte[] classData, String targetMethodName,
+			List<Type> targetMethodParameters, Type targetMethodReturnType) {
+		this.lambdaClassName = lambdaClassName;
+		this.classData = classData;
+		this.targetMethodName = targetMethodName;
+		this.targetMethodParameters = targetMethodParameters;
+		this.targetMethodReturnType = targetMethodReturnType;
+	}
 
-    public String getLambdaClassName() {
-        return lambdaClassName;
-    }
+	public String getLambdaClassName() {
+		return lambdaClassName;
+	}
 
-    public byte[] getClassData() {
-        return classData;
-    }
+	public byte[] getClassData() {
+		return classData;
+	}
 
-    public String getTargetMethodName() {
-        return targetMethodName;
-    }
+	public String getTargetMethodName() {
+		return targetMethodName;
+	}
 
-    public List<Type> getTargetMethodParameters() {
-        return targetMethodParameters;
-    }
+	public List<Type> getTargetMethodParameters() {
+		return targetMethodParameters;
+	}
 
-    public Type getTargetMethodReturnType() {
-        return targetMethodReturnType;
-    }
+	public Type getTargetMethodReturnType() {
+		return targetMethodReturnType;
+	}
 }

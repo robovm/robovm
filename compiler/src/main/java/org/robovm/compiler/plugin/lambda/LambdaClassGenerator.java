6b47a2552ebc85669ed191cb008167d95ea29573
Niklas Therning <niklas@therning.org>
2015-09-04 17:24:09 +0200

    Handle unboxing of Object/Number when expected type is a primitive. (robovm/robovm#1141)

--- compiler/src/main/java/org/robovm/compiler/plugin/lambda/LambdaClassGenerator.java
+++ compiler/src/main/java/org/robovm/compiler/plugin/lambda/LambdaClassGenerator.java
@@ -16,32 +16,7 @@
  */
 package org.robovm.compiler.plugin.lambda;
 
-import static org.objectweb.asm.Opcodes.ACC_BRIDGE;
-import static org.objectweb.asm.Opcodes.ACC_FINAL;
-import static org.objectweb.asm.Opcodes.ACC_PRIVATE;
-import static org.objectweb.asm.Opcodes.ACC_PUBLIC;
-import static org.objectweb.asm.Opcodes.ACC_STATIC;
-import static org.objectweb.asm.Opcodes.ACC_SUPER;
-import static org.objectweb.asm.Opcodes.ACC_SYNTHETIC;
-import static org.objectweb.asm.Opcodes.ALOAD;
-import static org.objectweb.asm.Opcodes.ARETURN;
-import static org.objectweb.asm.Opcodes.DLOAD;
-import static org.objectweb.asm.Opcodes.DRETURN;
-import static org.objectweb.asm.Opcodes.DUP;
-import static org.objectweb.asm.Opcodes.FLOAD;
-import static org.objectweb.asm.Opcodes.FRETURN;
-import static org.objectweb.asm.Opcodes.GETFIELD;
-import static org.objectweb.asm.Opcodes.ILOAD;
-import static org.objectweb.asm.Opcodes.INVOKEINTERFACE;
-import static org.objectweb.asm.Opcodes.INVOKESPECIAL;
-import static org.objectweb.asm.Opcodes.INVOKESTATIC;
-import static org.objectweb.asm.Opcodes.INVOKEVIRTUAL;
-import static org.objectweb.asm.Opcodes.IRETURN;
-import static org.objectweb.asm.Opcodes.LLOAD;
-import static org.objectweb.asm.Opcodes.LRETURN;
-import static org.objectweb.asm.Opcodes.NEW;
-import static org.objectweb.asm.Opcodes.PUTFIELD;
-import static org.objectweb.asm.Opcodes.RETURN;
+import static org.objectweb.asm.Opcodes.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -257,6 +232,12 @@ public class LambdaClassGenerator {
             if (isBoxedType(expected)) {
                 caster.box(expectedAsmType);
             }
+        } else if (isPrimitiveType(expected) && actual instanceof RefType) {
+            // actual could be java.lang.Number or even java.lang.Object when
+            // expected is a primitive. We need to unbox in this case too.
+            // unbox() will do the right thing, e.g. ((Number) v).longValue() if
+            // expected is long.
+            caster.unbox(getAsmPrimitiveType(expected));
         } else {
             // simple cast which will throw a ClassCastException at runtime
             mv.visitTypeInsn(Opcodes.CHECKCAST, Types.getInternalName(expected));
